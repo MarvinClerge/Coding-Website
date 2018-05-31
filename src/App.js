4f450c0dcb50102b437fbff5d05a36332784f5c5
Marvin Clerge <clergemarvin@gmail.com>
2018-05-31 00:03:53 -0400

    mobile css complete

--- src/App.js
+++ src/App.js
@@ -32,39 +32,27 @@ class App extends Component {
             <div>
               <Welcome />
 
-              <div className='section'>
-                <div className="section-text">
+              <div className='section d-grey'>
+                <div>
                   <h4>Write Code</h4>
                   <p>Write and run code using the text editor</p>
                   <p>Save and manage your code</p>
                 </div>
-
-                <div className='section-img'>
-                  <img src="https://www.usnews.com/dims4/USNEWS/f462862/2147483647/thumbnail/970x647/quality/85/?url=http%3A%2F%2Fmedia.beam.usnews.com%2Fea%2F1a%2F48eb43bf4e708c1e55d6de41dfc1%2F141212-computer-submitted.jpg" alt="code"/>
-                </div>
               </div>
 
-              <div className='section2'>
-                <div className='section2-img'>
-                  <img src="http://absolutetransitions.com/wp-content/uploads/2012/03/Computers-300x225.jpg" alt="challenge"/>
-                </div>
-
-                <div className="section2-text">
+              <div className='section l-grey'>
+                <div>
                   <h4>Take Challenges</h4>
                   <p>Take on challenges from our community</p>
                   <p>Build customs challenges and put others to the test</p>
                 </div>
               </div>
 
-              <div className='section'>
-                <div className="section-text">
+              <div className='section d-grey'>
+                <div>
                   <h4>Third thing</h4>
                   <p>Can't think of anything yet</p>
                   <p>It will be awesome when I do</p>
-                </div>
-
-                <div className='section-img'>
-                  <img src="https://i.imgur.com/s4dTtBy.jpg" alt="shrug"/>
                 </div>
               </div>
 
